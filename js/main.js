45aa1a7bb7baf813d98eb8095a8a0d3d072d4ca9
ngrosso1 <nicholasjgrosso@protonmail.com>
2022-03-13 04:45:43 -0400

    Added limits and multi line guesses

--- js/main.js
+++ js/main.js
@@ -4,14 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let guessedWords = [[]];
     let availableSpace = 1;
-
-    for (let i = 0; i < keyz.length; ++i) {
-        keyz[i].onclick = ({ target }) => {
-            const key = target.getAttribute("data-key");
-
-            updateGuessedWords(key);
-        };
-    }
+    let word = "dairy";
 
     function getCurrentWordsArr(){
         const numbOfGuessedWords = guessedWords.length;
@@ -23,11 +16,29 @@ document.addEventListener("DOMContentLoaded", () => {
         if (currentWordsArr && currentWordsArr.length < 5) {
             currentWordsArr.push(letter);
 
-            const availableSpaceEl = document.getElementById(String(1));
+            const availableSpaceEl = document.getElementById(String(availableSpace));
             availableSpace = availableSpace + 1;
 
             availableSpaceEl.textContent = letter;
         }
+    }
+
+    function handleSubmitWords(){
+        const currentWordsArr = getCurrentWordsArr();
+        if (currentWordsArr.length !== 5) {
+            window.alert("Please enter a word of 5 letters");
+        }
+
+        const currentWord = currentWordsArr.join("");
+        if (currentWord === word) {
+            window.alert("Correct!");
+        }
+
+        if (guessedWords.length === 6) {
+            window.alert("You have used all your guesses\nThe word was: " + word);
+        }
+
+        guessedWords.push([]);
     }
 
     function setUpSquares() {
@@ -38,8 +49,18 @@ document.addEventListener("DOMContentLoaded", () => {
             squares.setAttribute("id", i + 1);
             game.appendChild(squares);
         }
-
     }
 
+    for (let i = 0; i < keyz.length; ++i) {
+        keyz[i].onclick = ({ target }) => {
+            const key = target.getAttribute("data-key");
 
+            if(key === 'enter'){
+                handleSubmitWords()
+                return;
+            }
+
+            updateGuessedWords(key);
+        };
+    }
 })
