15f13cb17ecef723676268eced1b802e147657e6
ngrosso1 <nicholasjgrosso@protonmail.com>
2022-03-13 05:39:29 -0400

    Guesses flip colors

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let guessedWords = [[]];
     let availableSpace = 1;
     let word = "dairy";
+    let guessedWordCount = 0;
 
     function getCurrentWordsArr(){
         const numbOfGuessedWords = guessedWords.length;
@@ -23,6 +24,22 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    function getTileColor(letter, index) {
+        const correct = word.includes(letter);
+        if (!correct){
+            return "rgb(58, 58, 60)";
+        }
+
+        keyInPos = word.charAt(index);
+        const isCorrectPos = letter === keyInPos;
+
+        if (isCorrectPos){
+            return "rgb(83, 141, 78)";
+        }
+
+        return "rgb(181, 159, 59)";
+    }
+
     function handleSubmitWords(){
         const currentWordsArr = getCurrentWordsArr();
         if (currentWordsArr.length !== 5) {
@@ -30,6 +47,22 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         const currentWord = currentWordsArr.join("");
+
+        const firstID = guessedWordCount * 5 + 1;
+        const wait = 200;
+
+        currentWordsArr.forEach((letter, index) => {
+            setTimeout(() => {
+                const tileColor = getTileColor(letter, index);
+                const letterID = firstID + index; 
+                const letterEl = document.getElementById(letterID);
+                letterEl.classList.add("animate__flipInX");
+                letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
+            }, wait * index);
+        })
+
+        guessedWordCount += 1;
+
         if (currentWord === word) {
             window.alert("Correct!");
         }
@@ -46,6 +79,7 @@ document.addEventListener("DOMContentLoaded", () => {
         for(let i = 0; i < 30; ++i){
             let squares = document.createElement("div");
             squares.classList.add("squares");
+            squares.classList.add("animate__animated");
             squares.setAttribute("id", i + 1);
             game.appendChild(squares);
         }
